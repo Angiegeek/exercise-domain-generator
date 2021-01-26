047fdd1748912b228269a0cbae7bd824f0d19942
Óscar Sánchez <oscarsanchezmeza97@gmail.com>
2021-01-26 21:14:54 +0000

    Domain name generator created

---
+++ index.js
@@ -0,0 +1,20 @@
+document.querySelector('#excuse').addEventListener('mouseover', changeTheExcuse);
+
+function changeTheExcuse() {
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+
+    let whoD = pronoun[random()];
+    let actionD = adj[random()];
+    let whatD = noun[random()];
+
+    let excuse = `${whoD} ${actionD} ${whatD}.com`;
+
+    let paragraph = document.querySelector('#excuse');
+    paragraph.innerHTML = excuse;
+
+}
+function random(){
+    return Math.floor(Math.random() * (1 - 0) + 0);
+}
